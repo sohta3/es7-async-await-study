3ef60c352cfbac10979c15a845f271054f3c62eb
Sogo Ohta <sogo.ohta@eure.jp>
2015-09-28 19:33:18 +0900

    Add callback and promise

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,53 @@
 
 import "babel/polyfill";
 
-async function asyncCountUp() {
-  console.log('[begin]');
+function callbackCountUp() {
+  console.log('**[begin]');
   var n = 0;
+
+  countUpWithCallback(n, function(result1) {
+    countUpWithCallback(result1, function(result2) {
+      countUpWithCallback(result2, function(result3) {
+        countUpWithCallback(result3, function(result4) {
+          countUpWithCallback(result4, function(result5) {
+            n = result5;
+            console.log('**[finished] ' + n);
+          });
+        });
+      });
+    });
+  });
+}
+
+function countUpWithCallback(n, callback) {
+  console.log('countUp');
+  setTimeout(() => {
+    callback(n + 1);
+  }, 1000);
+}
+
+function promiseCountUp() {
+  console.log('**[begin]');
+  var n = 0;
+
+  countUp(n).then((n) => {
+    return countUp(n);
+  }).then((n) => {
+    return countUp(n);
+  }).then((n) => {
+    return countUp(n);
+  }).then((n) => {
+    return countUp(n);
+  }).then((n) => {
+    console.log(n);
+    console.log('**[finished] ' + n);
+  });
+}
+
+async function asyncCountUp() {
+  console.log('**[begin]');
+  var n = 0;
+
   try {
     for(var i = 0; i < 5; i++) {
       n = await countUp(n);
@@ -13,8 +57,9 @@ async function asyncCountUp() {
     console.log(e);
     console.log('error happened!!!');
   }
+
   console.log(n);
-  console.log('[finished]');
+  console.log('**[finished]');
 }
 
 function countUp(n) {
@@ -26,4 +71,6 @@ function countUp(n) {
   })
 }
 
+module.exports.callbackCountUp = callbackCountUp;
+module.exports.promiseCountUp = promiseCountUp;
 module.exports.asyncCountUp = asyncCountUp;
